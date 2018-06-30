198a9c11c9c27c3b225075d215f7d2267a34b0b5
x89k <lduck11007@gmail.com>
2018-06-30 18:56:32 +1000

    fix coords

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def show(c1, c2):
     for i in range(len(grid)):
         terminal.addstr(i, 0, ''.join(grid[i]))
     terminal.addstr(len(grid) + 1, 0,
-                    'P1: ({}, {}), P2: ({}, {}). a1: {}, a2: {}, a1_v: {}, a2_v: {}         '.format(
+                    'P1: ({0: <3}, {1: <3}), P2: ({2: <3}, {3: <3}). a1: {4: <8}, a2: {5: <8}, a1_v: {6: <5}, a2_v: {7: <5}         '.format(
         x1 - 60,
         y1 - 30,
         x2 - 60,
